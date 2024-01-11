6dc173c6868f53e7cb0fd64e0ae21bdfcdfc5085
nishan.mahamalimage2 <nishan.mahamalimage2@sky.uk>
2024-01-11 15:07:33 +0000

    first commit

---
+++ inbuilt.rb
@@ -0,0 +1,11 @@
+puts "happy birthday".length 
+p nil
+puts "happy birthday".upcase 
+# you can also do .to_s to covert into string
+p 5.to_s
+
+name = "Nishan"
+p "Ello, my name is #{name}"
+
+age = 25
+p "I am " + age.to_s
---
+++ puts.rb
@@ -0,0 +1,48 @@
+puts "Hello World"
+
+# you can print integers
+puts 5 
+
+# you can print floats
+
+puts 3.141
+
+puts 3 + 4
+
+# you can add strings :o 
+puts "4" + "3"
+
+# print nothing? 
+puts 
+
+puts "multi line 
+puts "
+# no new lines with print
+print "Corona"
+print "Extra"
+puts 
+
+# print v puts, the showdown
+print 1,2,3
+puts
+puts 1,2,3
+
+p "Hi, am i a string?"
+
+# data type is retained despite remainder
+p 12 / 5
+
+# answer in float
+p 12.0 / 5
+
+# to the power
+p 2 ** 5
+
+# modulo 
+p 5 % 2 
+
+=begin
+this is how you write a
+ multi line comment 
+hehehe
+=end
