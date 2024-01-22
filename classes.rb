295e9a6f8fa4d8539751d23b92d135f1020caf4f
nishan9 <nishan.mahamalimage2@sky.uk>
2024-01-22 18:30:25 +0000

    up to classes

---
+++ classes.rb
@@ -0,0 +1,13 @@
+p 5.class
+p 5.class.superclass
+p 5.class.superclass.superclass
+p 5.class.superclass.superclass.superclass
+p 5.class.superclass.superclass.superclass.superclass
+
+p "-----"
+
+p 2.32.class
+p 2.32.class.superclass
+p 2.32.class.superclass.superclass
+p 2.32.class.superclass.superclass.superclass
+p 2.32.class.superclass.superclass.superclass.superclass
--- time.rb
+++ time.rb
@@ -5,3 +5,15 @@ puts someday.to_s
 puts someday.ctime
 
 p someday.to_a
+
+today = Time.now
+
+#custime dat/time formatting
+
+p today.strftime("%B %d %Y")
+
+require 'time'
+
+p Time.parse("2020-01-02")
+
+p Time.strptime("03-04-2000","%d-%m-%Y")
